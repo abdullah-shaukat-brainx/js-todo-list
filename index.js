1c23000cc705fde81c217438ff2998e2b0252dd7
abdullah-shaukat-brainx <166981476+abdullah-shaukat-brainx@users.noreply.github.com>
2024-05-09 16:51:48 +0500

    Removed the function( Array.prototype) index.js

--- index.js
+++ index.js
@@ -2,22 +2,6 @@ let taskInput = document.getElementById("task-input");
 let taskSubmit = document.getElementById("submit");
 let task_list_pointer = document.getElementById("task-list");
 
-// Custom function to make a method remove(), this function
-// takes an input and then remove that input from the array.
-Array.prototype.remove = function () {
-  var what,
-    a = arguments,
-    L = a.length,
-    ax;
-  while (L && this.length) {
-    what = a[--L];
-    while ((ax = this.indexOf(what)) !== -1) {
-      this.splice(ax, 1);
-    }
-  }
-  return this;
-};
-
 
 const taskList = new Array();
 
@@ -46,7 +30,7 @@ function addToTaskList(value) {
   let deleteButton = document.createElement("button");
   deleteButton.classList.add("delete-button");
   deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-  
+
   let doneButton = document.createElement("button");
   doneButton.classList.add("done-button");
   doneButton.innerHTML = '<i class="fa fa-check"></i>';
@@ -60,22 +44,16 @@ function addToTaskList(value) {
 
   taskList.push(li);
 
-
-
-
-
   doneButton.addEventListener("click", (e) => {
     e.preventDefault();
     let parent = doneButton.parentElement;
     let paragraph = parent.querySelector(".undone");
 
-    if(paragraph===null)
-    {
+    if (paragraph === null) {
       paragraph = parent.querySelector(".done");
       paragraph.classList.remove("done");
       paragraph.classList.add("undone");
-    }
-    else{
+    } else {
       paragraph.classList.remove("undone");
       paragraph.classList.add("done");
     }
@@ -86,7 +64,7 @@ function addToTaskList(value) {
     let parent = deleteButton.parentElement;
     let grandParent = parent.parentElement;
     grandParent.removeChild(parent);
-    taskList.remove(parent);
+    taskList.splice(taskList.indexOf(parent),1); //Just like  -> taskList.remove(parent);
   });
 
   editButton.addEventListener("click", (e) => {
@@ -107,25 +85,21 @@ function addToTaskList(value) {
     update.classList.add("visible");
     update.classList.remove("hidden");
 
-
-    editButton.style.display="none";
+    editButton.style.display = "none";
 
     updateButton.addEventListener("click", (e) => {
       e.preventDefault();
       do {
-
         paragraph.innerText = input.value;
 
         paragraph.classList.add("visible");
         paragraph.classList.remove("hidden");
 
-
         input.classList.add("hidden");
         input.classList.remove("visible");
         update.classList.add("hidden");
         update.classList.remove("visible");
-        editButton.style.display="block";
-
+        editButton.style.display = "block";
       } while (input.value === "");
       updateScreen();
     });
